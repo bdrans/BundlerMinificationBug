6d31acacb56e7bfdbc703b6ad5455bb0756b80ce
Benjamin Dransfield <benjamin.dransfield@gmail.com>
2024-01-18 20:45:19 -0700

    Add project files.

---
+++ BundlerMinificationBug/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BundlerMinificationBug.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Bundled()
+        {
+            return View();
+        }
+        public ActionResult UnBundled()
+        {
+            return View();
+        }
+    }
+}
---
+++ BundlerMinificationBug/Scripts/ForTest.js
@@ -0,0 +1,95 @@
+function forInConst(arr) {
+    for (const arr_item in arr) {
+        let ul = document.getElementById("forInConst");
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(arr_item));
+        ul.appendChild(li);
+    }
+}
+
+function forInVar(arr) {
+    for (var arr_item in arr) {
+        let ul = document.getElementById("forInVar");
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(arr_item));
+        ul.appendChild(li);
+    }
+}
+
+function forInLet(arr) {
+    for (let arr_item in arr) {
+        let ul = document.getElementById("forInLet");
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(arr_item));
+        ul.appendChild(li);
+    }
+}
+
+function forOfConst(arr) {
+    for (const arr_item of arr) {
+        let ul = document.getElementById("forOfConst");
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(arr_item));
+        ul.appendChild(li);
+    }
+}
+
+function forOfVar(arr) {
+    for (var arr_item of arr) {
+        let ul = document.getElementById("forOfVar");
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(arr_item));
+        ul.appendChild(li);
+    }
+}
+
+function forOfLet(arr) {
+    for (let arr_item of arr) {
+        let ul = document.getElementById("forOfLet");
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(arr_item));
+        ul.appendChild(li);
+    }
+}
+
+function DemoBug() {
+    let array = ["a", "b", "c"];
+    try {
+        forInConst(array);
+    }
+    catch (error) {
+        console.error(error);
+    }
+    try {
+        forInVar(array);
+    }
+    catch (error) {
+        console.error(error);
+    }
+    try {
+        forInLet(array);
+    }
+    catch (error) {
+        console.error(error);
+    }
+    try {
+        forOfConst(array);
+    }
+    catch (error) {
+        console.error(error);
+    }
+    try {
+        forOfVar(array);
+    }
+    catch (error) {
+        console.error(error);
+    }
+    try {
+        forOfLet(array);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+DemoBug();
